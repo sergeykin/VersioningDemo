c4cb4f1253222a0c4f3a7563d362d9e621b6ceef
sergeykin <sergeykinn@mail.ru>
2021-09-22 22:50:31 +0300

    2

--- src/main/java/org/ea/VersioningDemo.java
+++ src/main/java/org/ea/VersioningDemo.java
@@ -6,7 +6,7 @@ package org.ea;
 public class VersioningDemo {
     public static void main(String args[]) {
         System.out.println("Hello explorers.");
-        System.out.println();
+        //System.out.println();
         System.out.println("This program is running version "+
                 VersioningDemo.class.getPackage().getImplementationVersion());
     }
